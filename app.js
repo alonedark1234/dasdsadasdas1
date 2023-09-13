61fedf22b55bad9dc396db58f506390046a84d2a
alonedark1234 <82684511+alonedark1234@users.noreply.github.com>
2023-09-13 16:44:54 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const { Client } = require('discord.js');
+
+const client = global.client = new Client({ disableEveryone: true});
+const keepAlive = require("./server");
+const process = global.process;
+
+require('./src/login');
+require('./src/events');
+require('./src/commands');
+
+keepAlive();
+client.login
+ 
---
+++ src/config.js
@@ -0,0 +1,13 @@
+const config = { 
+//------------Connections-----------//
+    MongoDB_ConnectURL: 'mongodb+srv://alonedark:alonedark@cluster0.zhwul.mongodb.net/darkchat1?retryWrites=true&w=majority',
+    //------------BotSettings-----------//
+    Prefix: '!',
+    BotOwners: ["902943204540841984"],
+    Custom_Status_Text: 'Developed By Alone',
+    Custom_Status_Type: 'PLAYING', // => PLAYING / WATCHING / LISTENING
+    Custom_Status: 'dnd', // => dnd / idle / online / invisible
+    VoiceChannelID: '1148623378563993651'
+};
+  
+module.exports = config;
